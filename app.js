72cd00e830012888991ef9f6e69823aba8228691
ZephyrCZR <zephyr_i@outlook.com>
2020-03-04 23:09:34 +0800

    添加了初始用户词书的API

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require('path')
 const bodyParser = require('body-parser')
 
 // 数据库：
-const connect = require('./connect')
+const connect = require('./db/connect')
 
 
 // 路由：
-// const userInfo = require('./routers/userInfo')
+const word = require('./routers/word')
 
 const app = express()
 
@@ -24,15 +24,15 @@ const app = express()
 app.use('/public/', express.static(path.join(__dirname,'./public/'))) //path.join:拼接路径，并且处理斜杠
 app.use('/node_modules', express.static(path.join(__dirname, './node_modules/')))
 
-app.engine('html', require('express-art-template'))
-app.set('views', path.join(__dirname, './views/'))//默认就是./views
+// app.engine('html', require('express-art-template'))
+// app.set('views', path.join(__dirname, './views/'))//默认就是./views
 
 // 挂载post请求体插件，必须在app.use(router)之前
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(userInfo)
+app.use(word)
 
 app.listen(5000, () => {
-  console.log('running...');
+  console.log('running>> http://127.0.0.1:5000')
 })
---
+++ routers/word.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const db = require('../db/utils')
+const router = express.Router()
+
+//初始化用户词书
+router.get('/initbook', (req, res) => {
+  console.log(req.query); 
+
+  let userId = req.query.userId
+  let bookName = req.query.bookName
+  db.initUserBook(userId, bookName).then((message) => {
+    res.send(message)
+  },(error) => {
+    res.send(error)
+  })
+})
+
+
+module.exports = router
